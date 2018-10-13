f2e862e978e57ffdeefeb9455ddb2f310a800dd1
Robert Sadowksi <robertsadowski005@gmail.com>
2018-10-13 13:01:37 +0200

    Dodano cztery zadania; - na rysowanie linii. - na rysowanie kwadratu. - na rysowanie trójkąta. - na rysowanie X.

---
+++ src/main/java/AlgorithmsForDrawing/DrawTriangle.java
@@ -0,0 +1,30 @@
+package AlgorithmsForDrawing;
+
+import java.util.Scanner;
+
+public class DrawTriangle {
+    public static void main(String[] args) {
+        System.out.print("Enter int value: ");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        drawingTriange(number);
+    }
+
+    private static void drawingTriange(int number) {
+
+        for (int i = 0; i < number; i++){
+            System.out.println();
+            for(int j = 0; j < number; j++){
+//                System.out.print("");
+                if (i<=j) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+
+
+        }
+    }
+}
